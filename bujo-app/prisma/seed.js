463ffc44d7add7bcbffe82d258180f5329b206b4
Suvi <suvi.lindholm@edu.karelia.fi>
2024-04-09 15:44:26 +0300

    Update seed to correspond database

--- bujo-app/prisma/seed.js
+++ bujo-app/prisma/seed.js
@@ -1,58 +1,64 @@
-// import { PrismaClient } from "@prisma/client";
-const { PrismaClient } = require("@prisma/client"); //tällä toimii tarpeeksi pitkälle seed ei varsinaisesti vielä oikein
+// This is a seed file for Prisma
+
+// const { PrismaClient } = require("@prisma/client");
+import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
 
-async function main() {
-    const alice = await prisma.user.upsert({
-        where: { email: "alice@prisma.testi" },
-        update: {},
-        create: {
-            email: "alice@prisma.testi",
-            password: "aliceTesti1",
-            name: "Alice",
-            picture:
-                "https://images.freeimages.com/image/previews/e84/buzzy-bean-girl-smiling-cartoon-png-5690326.png?fmt=webp&h=350",
-            Bookshelf: {
-                create: [
-                    {
-                        title: "My Favorite Notes",
-                        description: "A collection of my favorite notes.",
-                    },
-                ],
-            },
+const usersData = [
+    {
+        username: "Jane",
+        password: "securepassword",
+        email: "jane@example.testi",
+        picture: "./public/1.png",
+        Bookshelf: {
+            create: [
+                {
+                    title: "My Favorite Books",
+                    description: "A collection of my favorite books.",
+                },
+            ],
         },
-    });
+    },
+    {
+        username: "Bob",
+        password: "testi123",
+        email: "bob@example.testi",
+        picture: "./public/2.png",
+        Bookshelf: {
+            create: [
+                {
+                    title: "Year 2024",
+                    description: "Collection of notes from year 2024",
+                },
+                {
+                    title: "Year 2023",
+                    description: "Collection of notes from year 2023",
+                },
+            ],
+        },
+    },
+];
 
-    const bob = await prisma.user.upsert({
-        where: { email: "bob@prisma.testi" },
-        update: {},
-        create: {
-            email: "bob@prisma.io",
-            password: "bobTesti2",
-            name: "Bob",
-            Bookshelf: {
-                create: [
-                    {
-                        title: "My life",
-                        description: "A collection of my notes.",
-                    },
-                    {
-                        title: "Working hard",
-                        description: "A collection of my study notes.",
-                    },
-                ],
+async function main() {
+    const userCreationPromises = usersData.map((userData) =>
+        prisma.user.create({
+            data: userData,
+            include: {
+                Bookshelf: true,
             },
-        },
-    });
-    console.log({ alice, bob });
+        })
+    );
+
+    const users = await Promise.all(userCreationPromises);
+    console.log(users);
 }
+
 main()
-    .then(async () => {
-        await prisma.$disconnect();
-    })
-    .catch(async (e) => {
+    .catch((e) => {
         console.error(e);
-        await prisma.$disconnect();
         process.exit(1);
+    })
+    .finally(async () => {
+        await prisma.$disconnect();
     });
