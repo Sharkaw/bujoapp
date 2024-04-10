3425237f6fec7d36d1646c83ea579c5d936852c4
Suvi <suvi.lindholm@edu.karelia.fi>
2024-04-10 15:33:09 +0300

    Change seed structure to more understanding form

--- bujo-app/prisma/seed.js
+++ bujo-app/prisma/seed.js
@@ -4,60 +4,127 @@ const { PrismaClient } = require("@prisma/client");
 
 const prisma = new PrismaClient();
 
-const usersData = [
-    {
-        username: "Jane",
-        password: "securepassword",
-        email: "jane@example.testi",
-        picture: "./public/1.png",
-        Bookshelf: {
-            create: [
-                {
-                    title: "My Favorite Books",
-                    description: "A collection of my favorite books.",
-                },
-            ],
-        },
-    },
-    {
-        username: "Bob",
-        password: "testi123",
-        email: "bob@example.testi",
-        picture: "./public/2.png",
-        Bookshelf: {
-            create: [
-                {
-                    title: "Year 2024",
-                    description: "Collection of notes from year 2024",
-                },
-                {
-                    title: "Year 2023",
-                    description: "Collection of notes from year 2023",
-                },
-            ],
-        },
-    },
-];
-
 async function main() {
-    const userCreationPromises = usersData.map((userData) =>
-        prisma.user.create({
-            data: userData,
-            include: {
-                Bookshelf: true,
+    const alice = await prisma.user.upsert;
+    ({
+        where: { email: "alice@example.test" },
+        update: {},
+        create: {
+            username: "Alice",
+            password: "securepassword",
+            email: "alice@example.test",
+            picture: "./public/1.png",
+            Bookshelf: {
+                create: [
+                    {
+                        title: "My Bookshelf",
+                        description: "A collection of my journals.",
+                        Journal: {
+                            create: [
+                                {
+                                    title: "My Year 2024",
+                                    Notes_collection: {
+                                        create: [
+                                            {
+                                                title: "Studies",
+                                                Notes_item: {
+                                                    create: [
+                                                        {
+                                                            title: "Friday 16.",
+                                                            content:
+                                                                "Aliquam nunc est, porta quis placerat sed, consequat ac orci. Mauris eget nibh imperdiet, imperdiet turpis sed, ornare tellus. Interdum et malesuada fames ac ante ipsum primis in faucibus. Aliquam diam tortor, vulputate ac turpis sed, interdum dignissim lacus. Nulla tempor luctus velit non dictum. Vestibulum congue in tellus a lobortis. Curabitur vehicula dolor id dolor tincidunt, ut eleifend risus facilisis. Aliquam pulvinar pretium pretium. Nullam luctus sodales sapien pharetra laoreet. Fusce nec imperdiet enim, ut egestas lacus. Nulla luctus nulla feugiat orci semper, eu viverra tortor facilisis. Suspendisse imperdiet eget felis eu tristique",
+                                                        },
+                                                        {
+                                                            title: "Monday 25.",
+                                                            content:
+                                                                "Aliquam nunc est, porta quis placerat sed, consequat ac orci. Mauris eget nibh imperdiet, imperdiet turpis sed, ornare tellus. Interdum et malesuada fames ac ante ipsum primis in faucibus. Aliquam diam tortor, vulputate ac turpis sed, interdum dignissim lacus. Nulla tempor luctus velit non dictum. Vestibulum congue in tellus a lobortis. Curabitur vehicula dolor id dolor tincidunt, ut eleifend risus facilisis. Aliquam pulvinar pretium pretium. Nullam luctus sodales sapien pharetra laoreet. Fusce nec imperdiet enim, ut egestas lacus. Nulla luctus nulla feugiat orci semper, eu viverra tortor facilisis. Suspendisse imperdiet eget felis eu tristique",
+                                                        },
+                                                    ],
+                                                },
+                                            },
+                                        ],
+                                    },
+                                    Sticky_notes_collection: {
+                                        create: [
+                                            {
+                                                title: "My Sticky Notes",
+                                                Sticky_note_item: {
+                                                    create: [
+                                                        {
+                                                            content:
+                                                                "Remember the milk!",
+                                                        },
+                                                        {
+                                                            content:
+                                                                "Call Alyse.",
+                                                        },
+                                                    ],
+                                                },
+                                            },
+                                        ],
+                                    },
+                                },
+                                {
+                                    title: "My Year 2023",
+                                    Notes_collection: {
+                                        create: [
+                                            {
+                                                title: "Studies",
+                                                Notes_item: {
+                                                    create: [
+                                                        {
+                                                            title: "Friday 16.",
+                                                            content:
+                                                                "Aliquam nunc est, porta quis placerat sed, consequat ac orci. Mauris eget nibh imperdiet, imperdiet turpis sed, ornare tellus. Interdum et malesuada fames ac ante ipsum primis in faucibus. Aliquam diam tortor, vulputate ac turpis sed, interdum dignissim lacus. Nulla tempor luctus velit non dictum. Vestibulum congue in tellus a lobortis. Curabitur vehicula dolor id dolor tincidunt, ut eleifend risus facilisis. Aliquam pulvinar pretium pretium. Nullam luctus sodales sapien pharetra laoreet. Fusce nec imperdiet enim, ut egestas lacus. Nulla luctus nulla feugiat orci semper, eu viverra tortor facilisis. Suspendisse imperdiet eget felis eu tristique",
+                                                        },
+                                                        {
+                                                            title: "Monday 25.",
+                                                            content:
+                                                                "Aliquam nunc est, porta quis placerat sed, consequat ac orci. Mauris eget nibh imperdiet, imperdiet turpis sed, ornare tellus. Interdum et malesuada fames ac ante ipsum primis in faucibus. Aliquam diam tortor, vulputate ac turpis sed, interdum dignissim lacus. Nulla tempor luctus velit non dictum. Vestibulum congue in tellus a lobortis. Curabitur vehicula dolor id dolor tincidunt, ut eleifend risus facilisis. Aliquam pulvinar pretium pretium. Nullam luctus sodales sapien pharetra laoreet. Fusce nec imperdiet enim, ut egestas lacus. Nulla luctus nulla feugiat orci semper, eu viverra tortor facilisis. Suspendisse imperdiet eget felis eu tristique",
+                                                        },
+                                                    ],
+                                                },
+                                            },
+                                        ],
+                                    },
+                                    Sticky_notes_collection: {
+                                        create: [
+                                            {
+                                                title: "My Sticky Notes",
+                                                Sticky_note_item: {
+                                                    create: [
+                                                        {
+                                                            content:
+                                                                "Remember the milk!",
+                                                        },
+                                                        {
+                                                            content:
+                                                                "Call Alyse.",
+                                                        },
+                                                    ],
+                                                },
+                                            },
+                                        ],
+                                    },
+                                },
+                            ],
+                        },
+                    },
+                ],
             },
-        })
-    );
+        },
+    });
 
-    const users = await Promise.all(userCreationPromises);
-    console.log(users);
+    console.log(`Database has been seeded. 🌱`);
+    console.log({ alice });
 }
 
 main()
-    .catch((e) => {
-        console.error(e);
-        process.exit(1);
-    })
-    .finally(async () => {
+    .then(async () => {
         await prisma.$disconnect();
+    })
+    .catch(async (e) => {
+        console.error(e);
+        await prisma.$disconnect();
+        process.exit(1);
     });
