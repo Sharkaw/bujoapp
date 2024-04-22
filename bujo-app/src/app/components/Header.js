ce1d1decd9e57bf7b2f4cc6348f9ec5b00287fbe
unknown <pinjaamanda2003@gmail.com>
2024-04-22 13:55:11 +0300

    Added layout and mainPage

--- bujo-app/src/app/components/Header.js
+++ bujo-app/src/app/components/Header.js
@@ -1,24 +1,3 @@
-import React from 'react';
+/*import React from 'react';
 import Link from 'next/link'
-
-const Header = () => {
-  return (
-    <header className="bg-blue-500 text-white py-4">
-      <div className="container mx-auto flex justify-between items-center">
-      <ul>
-      <li>
-        <Link href="/">Home</Link>
-      </li>
-      <li>
-        <Link href="/about">About Us</Link>
-      </li>
-      <li>
-        <Link href="/blog/hello-world">Blog Post</Link>
-      </li>
-    </ul>
-      </div>
-    </header>
-  );
-};
-
-export default Header;
+*/
