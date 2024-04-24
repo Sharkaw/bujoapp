f7c03de1e133fab162d33e76a476b76b92e6ad9d
Suvi <suvi.lindholm@edu.karelia.fi>
2024-04-24 11:05:46 +0300

    Relocate components

--- bujo-app/src/app/page.js
+++ bujo-app/src/app/page.js
@@ -1,16 +1,14 @@
-import Image from "next/image";
-import { BaseButton } from "./common/BaseButton";
-import { LongButton } from "./common/LongButton";
-import { IconButton } from "./common/IconButton";
+import { BaseButton } from "./components/common/BaseButton";
+import { LongButton } from "./components/common/LongButton";
+import { IconButton } from "./components/common/IconButton";
 import Sidebar from "./pages/sidebar/page";
-import React from 'react';
+import React from "react";
 
 const MainPage = () => {
-  return (
-    <div>
-      Hello World!
-    </div>
-    <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
+    return (
+        <div>
+            <div>Hello World!</div>
+            <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
                 <div className="bg-white">
                     <BaseButton title="Button" variant="primary" />
                     <BaseButton title="Button" variant="success" />
@@ -28,7 +26,9 @@ const MainPage = () => {
                     <IconButton />
                 </div>
                 <Sidebar />
-  );
+            </div>
+        </div>
+    );
 };
 
 export default MainPage;
--- bujo-app/src/app/pages/sidebar/page.js
+++ bujo-app/src/app/pages/sidebar/page.js
@@ -1,4 +1,4 @@
-import SidebarMenu from "@/app/components/SidebarMenu";
+import SidebarMenu from "@/app/components/layout/SidebarMenu";
 import { PrismaClient } from "@prisma/client";
 
 export default async function Sidebar() {
