634f44c2129ce46d56606d818c4c7122e9281d23
Suvi <suvi.lindholm@edu.karelia.fi>
2024-04-24 11:06:46 +0300

    add missing import

--- bujo-app/src/app/layout.js
+++ bujo-app/src/app/layout.js
@@ -1,21 +1,22 @@
-import React from 'react';
+import React from "react";
+import "./globals.css";
 // import Header from './components/Header';
 // import Footer from './components/Footer'
 
 const LayoutGrid = ({ children }) => {
-  return (
-    <html lang='en'>
-      <body>
-        <div className="bg-white text-black min-h-screen">
-          <div className="grid grid-cols-12 gap-4">
-            {/* <Header /> */}
-            <main>{children}</main>
-            {/* <Footer /> */}
-          </div>
-        </div>
-      </body>
-    </html>
-  );
+    return (
+        <html lang="en">
+            <body>
+                <div className="bg-white text-black min-h-screen">
+                    <div className="grid grid-cols-12 gap-4">
+                        {/* <Header /> */}
+                        <main>{children}</main>
+                        {/* <Footer /> */}
+                    </div>
+                </div>
+            </body>
+        </html>
+    );
 };
 
 export default LayoutGrid;
