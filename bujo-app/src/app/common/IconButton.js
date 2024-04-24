b5d50a025a2876dfcbc5e647966e7cbefebb04d0
Suvi <suvi.lindholm@edu.karelia.fi>
2024-04-24 10:38:12 +0300

    Style icon Button

--- bujo-app/src/app/common/IconButton.js
+++ bujo-app/src/app/common/IconButton.js
@@ -1,14 +1,14 @@
 "use client";
 import "./buttons.css";
-// import { FiX } from "react-icons/fi";
+import { FiX } from "react-icons/fi";
 
 export function IconButton({ onClick }) {
     return (
         <button
-            className={`bg-white text-base text-gray-800  m-1 px-3 py-1  hover:bg-gray-300 hover:border-gray-300  border-white shadow inline-block`}
+            className={`bg-white text-base text-gray-800  m-1 px-2 py-2  hover:bg-gray-300 hover:border-gray-300  border-white inline-block`}
             onClick={onClick}
         >
-            x{/* <FiX /> */}
+            <FiX />
         </button>
     );
 }
