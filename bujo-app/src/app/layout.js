ffffb7c76b52fd3cbaf3b3b9b496f31db55bfd9a
Suvi <suvi.lindholm@edu.karelia.fi>
2024-04-25 15:31:10 +0300

    Add sidebar and styling to layout

--- bujo-app/src/app/layout.js
+++ bujo-app/src/app/layout.js
@@ -1,19 +1,24 @@
 import React from "react";
 import "./globals.css";
+import Sidebar from "./pages/sidebar/page";
 // import Header from './components/Header';
 // import Footer from './components/Footer'
 
 const LayoutGrid = ({ children }) => {
     return (
         <html lang="en">
-            <body>
-                <div className="bg-white text-black min-h-screen">
-                    <div className="grid grid-cols-12 gap-4">
-                        {/* <Header /> */}
-                        <main>{children}</main>
-                        {/* <Footer /> */}
+            <body className="flex flex-col min-h-screen">
+                <header className="bg-gray-800 text-white p-4">Header</header>
+                <div className="grid grid-cols-12 gap-4">
+                    <div className="col-span-3 min-w-1 max-w-5">
+                        <Sidebar />
                     </div>
+                    <div className="col-span-6 ">
+                        <main class="flex-grow">{children}</main>
+                    </div>
+                    <div className="col-span-3 ">Content 3</div>
                 </div>
+                <footer className="bg-gray-800 text-white p-4">Footer</footer>
             </body>
         </html>
     );
