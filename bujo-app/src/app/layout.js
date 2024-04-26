3289a3628230f10fff485349bdf44157e0e357ab
Suvi <suvi.lindholm@edu.karelia.fi>
2024-04-26 11:35:19 +0300

    Adjust global styling

--- bujo-app/src/app/layout.js
+++ bujo-app/src/app/layout.js
@@ -1,22 +1,24 @@
 import React from "react";
 import "./globals.css";
-import Sidebar from "./pages/sidebar/page";
-// import Header from './components/Header';
-// import Footer from './components/Footer'
+import Sidebar from "./components/layout/sidebar/page";
+// import Header from './components/layout/Header';
+// import Footer from './components/layout/Footer'
 
 const LayoutGrid = ({ children }) => {
     return (
         <html lang="en">
-            <body className="flex flex-col min-h-screen">
+            <body className="flex flex-col">
                 <header className="bg-gray-800 text-white p-4">Header</header>
                 <div className="grid grid-cols-12 gap-4">
-                    <div className="col-span-3 min-w-1 max-w-5">
+                    <div className="col-span-3 bg-red-600 flex">
                         <Sidebar />
                     </div>
-                    <div className="col-span-6 ">
-                        <main class="flex-grow">{children}</main>
+                    <div className="col-span-6 bg-blue-400">
+                        <main class="flex justify-center">{children}</main>
                     </div>
-                    <div className="col-span-3 ">Content 3</div>
+                    <div className="col-span-3 bg-green-700 flex justify-center">
+                        Content 3
+                    </div>
                 </div>
                 <footer className="bg-gray-800 text-white p-4">Footer</footer>
             </body>
