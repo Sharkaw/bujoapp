b3fec9459a18456ac531d0cfe76220995b306ed9
Suvi <suvi.lindholm@edu.karelia.fi>
2024-04-26 11:33:46 +0300

    Adjust button styling

--- bujo-app/src/app/components/common/BaseButton.js
+++ bujo-app/src/app/components/common/BaseButton.js
@@ -3,7 +3,7 @@ import "./buttons.css";
 export function BaseButton({ title, variant, onClick }) {
     return (
         <button
-            className={`text-base  m-1 px-6 py-3 border-4 inline-block ${variant}`}
+            className={`text-base m-1 px-6 py-3 border-4 inline-block ${variant}`}
             onClick={onClick}
         >
             {title}
--- bujo-app/src/app/components/common/LongButton.js
+++ bujo-app/src/app/components/common/LongButton.js
@@ -3,7 +3,7 @@ import "./buttons.css";
 export function LongButton({ title, variant, onClick }) {
     return (
         <button
-            className={`text-base text-gray-800 m-1 px-3 py-3 w-full min-h-14 border-4 inline-block ${variant}`}
+            className={`text-base m-1 px-3 py-3 w-full min-h-14 border-4 inline-block ${variant}`}
             onClick={onClick}
         >
             {title}
