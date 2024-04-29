40cef7b4a5b668297b05a392b8d6485a917c1ce9
Suvi <suvi.lindholm@edu.karelia.fi>
2024-04-29 15:21:52 +0300

    Update login form to work with RHF validation

--- bujo-app/src/app/components/forms/LoginForm.js
+++ bujo-app/src/app/components/forms/LoginForm.js
@@ -1,39 +1,72 @@
 "use client";
 import { LongButton } from "@/app/components/common/LongButton";
+import { useForm } from "react-hook-form";
 
 export default function LoginForm() {
+    const {
+        register,
+        handleSubmit,
+        formState: { errors },
+    } = useForm();
+
+    const onSubmit = (data) => {
+        console.log(data);
+    };
+
     return (
-        <form className=" bg-white rounded mx-12 p-2 pb-8 mb-4">
-            <div className="mb-8 p-1 border-b">
+        <form
+            className=" bg-white rounded mx-12 p-2 pb-8 mb-4"
+            onSubmit={handleSubmit((data) => {
+                console.log(data);
+            })}
+        >
+            <div className="mb-4 p-1 border-b">
                 <label
                     className="block text-gray-700 text-sm font-bold mb-2"
-                    for="username"
+                    htmlFor="userName"
                 >
                     Username/email
                 </label>
                 <input
+                    {...register("userName", {
+                        required: "Please type username",
+                    })}
                     className="appearance-none border-none bg-transparent rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                    id="username"
+                    id="userName"
                     type="text"
                     placeholder="Username"
                 />
             </div>
-            <div className="mb-8 p-1 border-b">
+            <p className="text-red-700 font-light text-xs mb-2 min-h-4">
+                {errors.userName?.message}
+            </p>
+            <div className="mb-4 p-1 border-b">
                 <label
                     className="block text-gray-800 text-sm font-bold mb-2"
-                    for="password"
+                    htmlFor="password"
                 >
                     Password
                 </label>
                 <input
+                    {...register("password", {
+                        required: "Please type password",
+                    })}
                     className=" appearance-none border-none bg-transparent rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
                     id="password"
                     type="password"
                     placeholder="******************"
                 />
             </div>
+            <p className="text-red-700 font-light text-xs mb-2 min-h-4">
+                {errors.password?.message}
+            </p>
             <div className="flex flex-col justify-center items-center">
-                <LongButton title="Login" variant="success" />
+                <LongButton
+                    title="Login"
+                    variant="success"
+                    type="submit"
+                    // onClick={handleSubmit(onSubmit)}
+                />
                 <a
                     className="m-1 p-1 inline-block font-bold text-sm text-blue-500 hover:text-blue-800"
                     href="#"
