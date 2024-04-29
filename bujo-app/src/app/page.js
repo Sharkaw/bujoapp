09c60fa05398c8cb50a1c2ec1a603e0cabb74a7b
Suvi <suvi.lindholm@edu.karelia.fi>
2024-04-29 15:25:25 +0300

    Remove stuff from landing and redirect to login for now

--- bujo-app/src/app/page.js
+++ bujo-app/src/app/page.js
@@ -1,34 +1,13 @@
 import { BaseButton } from "./components/common/BaseButton";
 import { LongButton } from "./components/common/LongButton";
 import { IconButton } from "./components/common/IconButton";
-import Sidebar from "./pages/sidebar/page";
+import Sidebar from "./components/layout/sidebar/page";
 import React from "react";
+import { redirect } from "next/navigation";
+// import Form from "./pages/login/page";
 
 const MainPage = () => {
-    return (
-        <div>
-            <div>Hello World!</div>
-            <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-                <div className="bg-white">
-                    <BaseButton title="Button" variant="primary" />
-                    <BaseButton title="Button" variant="success" />
-                    <BaseButton title="Button" variant="danger" />
-                    <BaseButton title="Button" variant="disabled" />
-                </div>
-                <div className="bg-white">
-                    <LongButton title="Button" variant="primary" />
-                    <LongButton title="Button" variant="success" />
-                    <LongButton title="Button" variant="danger" />
-                    <LongButton title="Button" variant="disabled" />
-                </div>
-
-                <div className="bg-white">
-                    <IconButton />
-                </div>
-                <Sidebar />
-            </div>
-        </div>
-    );
+    return <div>{redirect("/login")}</div>;
 };
 
 export default MainPage;
