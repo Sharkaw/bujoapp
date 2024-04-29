57070c2532c15b3f4ab352dc59a4def97d0b1fa2
Suvi <suvi.lindholm@edu.karelia.fi>
2024-04-29 15:26:31 +0300

    Remove unneeded imports

--- bujo-app/src/app/page.js
+++ bujo-app/src/app/page.js
@@ -1,10 +1,5 @@
-import { BaseButton } from "./components/common/BaseButton";
-import { LongButton } from "./components/common/LongButton";
-import { IconButton } from "./components/common/IconButton";
-import Sidebar from "./components/layout/sidebar/page";
 import React from "react";
 import { redirect } from "next/navigation";
-// import Form from "./pages/login/page";
 
 const MainPage = () => {
     return <div>{redirect("/login")}</div>;
