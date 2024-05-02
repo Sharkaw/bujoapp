9ba31b68ffe97d7fe1618e90fde1e676a0d6b3a8
Suvi <suvi.lindholm@edu.karelia.fi>
2024-05-02 12:31:25 +0300

    Remove bg colors

--- bujo-app/src/app/layout.js
+++ bujo-app/src/app/layout.js
@@ -10,14 +10,14 @@ const LayoutGrid = ({ children }) => {
             <body className="flex flex-col">
                 <header className="bg-gray-800 text-white p-4">Header</header>
                 <div className="flex flex-col md:flex-row flex-1">
-                    <div className="hidden min-w-60 md:flex md:flex-auto bg-red-500">
+                    <div className="hidden min-w-60 md:flex md:flex-none">
                         <Sidebar />
                     </div>
-                    <main className="flex-auto flex justify-center bg-blue-500">
+                    <main className="flex-auto flex justify-center">
                         {children}
                     </main>
-                    <div className="flex-auto flex justify-center bg-green-500">
-                        Content 3
+                    <div className="flex-auto flex justify-center">
+                        {/* Content 3 */}
                     </div>
                 </div>
                 <footer className="bg-gray-800 text-white p-4">Footer</footer>
