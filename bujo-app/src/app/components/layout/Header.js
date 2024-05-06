a46484cbf418d3e71bd8f9618547d49f6323e9b7
unknown <pinjaamanda2003@gmail.com>
2024-05-06 09:41:34 +0300

    Move items to middle

--- bujo-app/src/app/components/layout/Header.js
+++ bujo-app/src/app/components/layout/Header.js
@@ -1,17 +1,19 @@
 import Link from 'next/link'
 import profile from './profile.png'
 
-const Header = () => {
+export default function Header() {
   return (
     <div>
-        <div className="container mx-auto flex items-center px-10 py-2 h-24 bg-gray-800">
-            <div className="flex items-center justify-center gap-6 nd:gap-8">
-                <Link href="/">BuJo</Link>
+        <div className="container mx-auto flex items-center px-10 py-2 h-24 bg-gray-300 justify-center">
+            <div className="flex items-center gap-6 nd:gap-8 text-gray-800 justify-center">
+                <Link href="/">
+                    <h1 className="mt-8">BuJo</h1>
+                </Link>
                 <Link legacyBehavior href="/profile">
                     <a>
                     <img
                     src={profile}
-                    alt="Profile picture"
+                    alt="Profile"
                     className="h-12 w-12 rounded-full cursor-pointer"
                     />
                     </a>
@@ -21,5 +23,3 @@ const Header = () => {
     </div>
   );
 };
-
-export default Header;
