b94260d6a83ae0990f36bf33cddd6a17fa91d553
unknown <pinjaamanda2003@gmail.com>
2024-05-13 15:38:01 +0300

    Add buttons

--- bujo-app/src/app/profile/page.js
+++ bujo-app/src/app/profile/page.js
@@ -1,34 +1,36 @@
+import React from 'react';
 import Image from "next/image";
-import profile from "../components/layout/profile.png";
-import "../components/layout/styles.css";
-import { BaseButton } from "../components/common/BaseButton";
+import { LongButton } from "@/app/components/common/LongButton";
+import { BaseButton } from "@/app/components/common/BaseButton";
+import { IconButton } from "@/app/components/common/IconButton";
+import profile from "@/assets/profile.png";
 
-export default function Profile() {
-    return (
+const ProfilePage = () => {
+  return (
+    <div className="flex flex-col md:flex-row w-full max-w-[1000px] mb-10">
+      <div className="flex flex-col mt-5 md:w-3/5 mx-auto">
+        <h1 className="text-stone-900 mb-5">Personal info</h1>
+        <p className="text-stone-900 mb-5 ml-10">Email: example@email.com</p>
+        <p className="text-stone-900 mb-5 ml-10">Password: ******</p>
+        <div className="w-48 ml-10"><LongButton title="Change password" variant="success" type="submit" /></div>
+      </div>
+      <div className="flex flex-col w-full md:w-1/3 justify-around mx-auto max-w-80 mt-10 md:mt-0 pr-3">
         <div>
-            <h1 className="mt-8">Personal info</h1>
-            <p className="p-1 m-1 inline-block align-baseline font-bold text-sm">
-                Email: 
-            </p>
-            <p className="p-1 m-1 inline-block align-baseline font-bold text-sm">
-                Password: 
-            </p>
-            <BaseButton title="Change password" variant="success" type="submit" />
-            <div className="flex-end m-1 p-1">
-                <Image
-                    src={profile}
-                    alt="Profile"
-                    width={48}
-                    height={48}
-                    className="rounded-full"
-                />
-                <p className="p-1 m-1 inline-block align-baseline font-bold text-sm">
-                Username
-                </p>
-                <BaseButton title="Edit photo" variant="success" type="submit" />
-                <BaseButton title="Save profile" variant="success" type="submit" />
-                <BaseButton title="Delete profile" variant="success" type="submit" />
-            </div>
+          <div className="border-black border-2 max-w-40 md:max-w-60 mx-auto">
+            <Image src={profile} alt="user" className=" rounded-full" />
+          </div>
+          <p className="w-full text-stone-900 my-5 text-center font-bold">Username</p>
+          <div className="w-1/2 mx-auto">
+          <BaseButton title="Edit photo" variant="primary" type="submit" />
+          </div>
         </div>
-    );
-}
+        <div className="flex flex-col w-11/12 mt-10 md:mt-0 justify-between min-h-36">
+          <BaseButton title="Save profile" variant="success" type="submit" />
+          <BaseButton title="Delete profile" variant="danger" type="submit" />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ProfilePage;
