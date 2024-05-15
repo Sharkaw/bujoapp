c7265b25e05975fe7b1a97e47eafa4f1d4bd1bf0
pp-shrek <153538284+pp-shrek@users.noreply.github.com>
2024-05-15 11:35:10 +0300

    Footer style fix (#17)
    * Footer style fix
    
    * font color change
    
    * change text color dark gray

--- bujo-app/src/app/components/layout/Footer.js
+++ bujo-app/src/app/components/layout/Footer.js
@@ -7,32 +7,31 @@ import insta from "@/assets/insta-small.png";
 
 export default function Footer() {
     return (
-        <footer className="bg-gray-800 p-5">
+        <footer className="bg-gray-800 p-5 md:pt-12 text-white">
             <div className="flex flex-col md:flex-row">
-                <div className="w-full md:w-1/5 flex justify-start md:justify-center items-center h-32">
+                <div className="md:w-1/5 flex justify-center">
                     <Link href="/"><h1>BuJo</h1></Link>
                 </div>
-                <div className="flex flex-wrap content-start w-full md:w-3/5 max-w-[900px] p-5">
-                <nav className="w-full">
-                        <ul className="flex flex-wrap content-start w-full">
+                <div className="flex justify-center w-full md:w-3/5 max-w-[900px] pb-5">
+                    <nav className="">
+                        <ul className="flex flex-col md:flex-row flex-wrap">
                             <li className="w-full md:w-1/2 pb-3 font-inter text-sm"><Link className="hover:underline" href="/">Contact us</Link></li>
                             <li className="w-full md:w-1/2 pb-3 font-inter text-sm"><Link className="hover:underline" href="/">Terms & Conditions</Link></li>
                             <li className="w-full md:w-1/2 pb-3 font-inter text-sm"><Link className="hover:underline" href="/">About us</Link></li>
                             <li className="w-full md:w-1/2 pb-3 font-inter text-sm"><Link className="hover:underline" href="/">Privacy Policy</Link></li>
                             <li className="w-full md:w-1/2 pb-3 font-inter text-sm"><Link className="hover:underline" href="/">Terms of use</Link></li>
                             <li className="w-full md:w-1/2 pb-3 font-inter text-sm"><Link className="hover:underline" href="/">Support</Link></li>
-                            
                         </ul>
                     </nav>
                 </div>
-                <div className="flex flex-wrap w-full md:w-1/4 bg-gray-800 gap-2 mt-0 md:mt-5">
-                    <a href="#"><Image src={fb} alt="facebook" className="h-9 w-9" /></a>
+                <div className="flex flex-wrap md:w-1/4 bg-gray-800 gap-2 mt-0 md:mt-5 justify-center">
+                    <a href="#" className=""><Image src={fb} alt="facebook" className="h-9 w-9" /></a>
                     <a href="#"><Image src={insta} alt="instagram" className="h-9 w-9" /></a>
                     <a href="#"><Image src={fb} alt="facebook" className="h-9 w-9" /></a>
                     <a href="#"><Image src={insta} alt="instagram" className="h-9 w-9" /></a>
                 </div>
             </div>
-            <div className="flex justify-center w-full my-3">
+            <div className="flex justify-center w-full mt-10">
                 <span className="font-kalam text-xl">&copy; Copyright</span>
             </div>
         </footer>
