974faba243a329eac85b248b89307286cecd6ffe
pp-shrek <153538284+pp-shrek@users.noreply.github.com>
2024-05-17 12:33:02 +0300

    Mobile menu versio 2 menu icon fix

--- bujo-app/src/app/components/common/HamburgerMenuButton.js
+++ bujo-app/src/app/components/common/HamburgerMenuButton.js
@@ -1,5 +1,10 @@
 "use client";
 
+import { HiOutlineMenu } from "react-icons/hi";
+import { MdOutlineClose } from "react-icons/md";
+
+
+
 function handleMobileMenu() {
     document.getElementById("sidebar")?.classList.toggle("hidden");
     document.getElementById("openMobileMenuBtn")?.classList.toggle("hidden");
@@ -14,14 +19,10 @@ export function HamburgerMenuButton() {
     return (
         <div id="sidebarMobileBtn" className="flex px-4 cursor-pointer md:hidden">
             <div id="openMobileMenuBtn" onClick={handleMobileMenu}>
-                <svg class="w-8 h-8 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
-                    <path stroke="currentColor" stroke-linecap="round" stroke-width="2" d="M5 7h14M5 12h14M5 17h14"/>
-                </svg>
+                <HiOutlineMenu className="w-8 h-8" />
             </div>
             <div id="closeMobileMenuBtn" className="hidden" onClick={handleMobileMenu}>
-                <svg class="w-8 h-8 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
-                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18 17.94 6M18 18 6.06 6"/>
-                </svg>
+                <MdOutlineClose className="w-8 h-8" />
             </div>
         </div>
     );
