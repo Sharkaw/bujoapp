1a8ccaa8f0506f2c0d805740f5111404a25c98fb
Suvi <suvi.lindholm@edu.karelia.fi>
2024-06-06 10:16:17 +0300

    Clean tests

--- bujo-app/src/app/profile/__tests__/Profile.test.jsx
+++ bujo-app/src/app/profile/__tests__/Profile.test.jsx
@@ -1,9 +1,6 @@
-import { describe, expect, beforeAll, it, vi, beforeEach } from "vitest";
-import { render, screen, waitFor } from "@testing-library/react";
+import { describe, expect, beforeAll, it } from "vitest";
+import { render, screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
-import { UpdateUserData } from "@/app/actions";
-import UpdateUserForm from "../../components/forms/UpdateUserForm";
-import { modal } from "@/app/components/modals/avatarModal";
 import ProfilePage from "@/app/profile/ProfilePage";
 
 const user = userEvent.setup();
@@ -15,7 +12,7 @@ const mockUser = {
     picture: "/profileimages/1.png",
 };
 
-describe("Update user data", () => {
+describe("Profile", () => {
     beforeAll(() => {
         render(<ProfilePage user={mockUser} />);
     });
@@ -46,20 +43,6 @@ describe("Update user data", () => {
         expect(decodeURIComponent(originalSrc)).toBe("/profileimages/1.png");
     });
 
-    it.todo("opens the avatar modal", async () => {
-        const chooseAvatarButton = screen.getByRole("button", {
-            name: /choose avatar/i,
-        });
-
-        await user.click(chooseAvatarButton);
-
-        // const selectButton = screen.getByRole("button", { name: /select/i });
-
-        // expect(screen.getByTestId(selectButton)).toBeInTheDocument();
-
-        expect(screen.getByTestId("select-button")).toBeInTheDocument();
-    });
-    it.todo("Should change profile image", async () => {});
     it.todo("Should change only username", async () => {});
     it.todo("Should change only email", async () => {});
 });
