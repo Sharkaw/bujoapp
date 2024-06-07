79446d088b66e8235d616149aa7a6695332f6500
Suvi <suvi.lindholm@edu.karelia.fi>
2024-06-07 12:29:43 +0300

    Testing

--- bujo-app/src/app/components/layout/Header.jsx
+++ bujo-app/src/app/components/layout/Header.jsx
@@ -6,33 +6,30 @@ import { HamburgerMenuButton } from "../common/HamburgerMenuButton";
 import ProfileImage from "./ProfileImage";
 import DropdownMenu from "./DropdownMenu";
 import "./styles.css";
+import { toggleTheme } from "@/app/actions";
 
-export default function Header() {
+export default function Header({ initialIsDarkMode }) {
     const [isDropdownOpen, setIsDropdownOpen] = useState(false);
-    const [isDarkMode, setIsDarkMode] = useState(false);
+    const [isDarkMode, setIsDarkMode] = useState(initialIsDarkMode);
 
     useEffect(() => {
-        const savedTheme = localStorage.getItem('theme');
-        if (savedTheme) {
-            setIsDarkMode(savedTheme === 'dark');
-        }
-    }, []);
+        document.body.className = initialIsDarkMode ? "dark" : "light";
+    }, [initialIsDarkMode]);
 
-    useEffect(() => {
-        document.body.className = isDarkMode ? 'dark' : 'light';
-        localStorage.setItem('theme', isDarkMode ? 'dark' : 'light');
-    }, [isDarkMode]);
+    const handleToggleTheme = async () => {
+        const newTheme = await toggleTheme();
+        setIsDarkMode(newTheme);
+        document.body.className = newTheme ? "dark" : "light";
+    };
 
     const handleProfileClick = () => {
         setIsDropdownOpen(!isDropdownOpen);
     };
 
-    const toggleTheme = () => {
-        setIsDarkMode(!isDarkMode);
-    };
-
     return (
-        <div className={`header flex items-center justify-between p-2 w-full h-24 bg-gray-50 dark:bg-gray-800 border-b-slate-300 border-solid border-b-4`}>
+        <div
+            className={`header flex items-center justify-between p-2 w-full h-24 bg-gray-50 dark:bg-gray-800 border-b-slate-300 border-solid border-b-4`}
+        >
             <Link href="/" className="flex-1 text-center">
                 <span className="text-5xl font-kalam text-gray-800 dark:text-gray-200 mb-0 p-2 block">
                     BuJo
@@ -42,7 +39,12 @@ export default function Header() {
                 <div onClick={handleProfileClick} className="cursor-pointer">
                     <ProfileImage />
                 </div>
-                {isDropdownOpen && <DropdownMenu isDarkMode={isDarkMode} toggleTheme={toggleTheme} />}
+                {isDropdownOpen && (
+                    <DropdownMenu
+                        isDarkMode={isDarkMode}
+                        toggleTheme={handleToggleTheme}
+                    />
+                )}
             </div>
             <HamburgerMenuButton />
         </div>
